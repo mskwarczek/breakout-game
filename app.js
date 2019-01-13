e06cbb98f1ca21e0b01de880bf51d343148a8855
Maciej Skwarczek <m.skwarczek@gmail.com>
2019-01-13 13:29:31 +0100

    add destroyable brick walls and points counter

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 'use strict';
 
-// === GAME ENVIRONMENT === //
-
 // Global variables
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+let gameState = {
+    level: 1,
+    score: 0
+}
 
 const field = {
     size: {
@@ -12,7 +15,7 @@ const field = {
         y: canvas.height
     },
     fillStyle: '#ccc'
-}
+};
 
 // Game objects prototypes
 class Paddle {
@@ -58,12 +61,36 @@ class Ball {
     moveBall() {
         this.position.x += this.speed.dx;
         this.position.y += this.speed.dy;
-    }
+    };
+};
+
+class Level {
+    constructor() {
+        this.brickRowCount = 3;
+        this.brickWidth = 75;
+        this.brickHeight = 25;
+        this.brickPadding = 10;
+        this.brickOffsetTop = 30;
+        this.brickOffsetLeft = 30;
+        this.brickOffsetRight = 30;
+        this.brickColumnCount = Math.floor((field.size.x - this.brickOffsetLeft - this.brickOffsetRight) / (this.brickWidth + this.brickPadding));
+        this.bricks = [];
+        this.generateBricks();
+    };
+    generateBricks() {
+        for (let col = 0; col < this.brickColumnCount; col++) {
+            this.bricks[col] = [];
+            for (let row = 0; row < this.brickRowCount; row++) {
+                this.bricks[col][row] = { x: 0, y: 0, strength: 1 };
+            };
+        };
+    };
 };
 
 // Create game objects
-let paddle = new Paddle();
-let ball = new Ball();
+let paddle = new Paddle;
+let ball = new Ball;
+let level = new Level;
 
 // Draw  field and objects
 function drawField() {
@@ -90,6 +117,29 @@ function drawBall() {
     ctx.closePath();
 };
 
+function drawGameState() {
+    ctx.font = '16px Arial';
+    ctx.fillStyle = '#000';
+    ctx.fillText('Level: ' + gameState.level, field.size.x - 80, 20)
+    ctx.fillText('Score: ' + gameState.score, 10, 20);
+}
+
+function drawBricks() {
+    for (let col = 0; col < level.brickColumnCount; col++) {
+        for (let row = 0; row < level.brickRowCount; row++) {
+            if (level.bricks[col][row].strength > 0) {
+                level.bricks[col][row].x = (col * (level.brickWidth + level.brickPadding) + level.brickOffsetLeft);
+                level.bricks[col][row].y = (row * (level.brickHeight + level.brickPadding) + level.brickOffsetTop);
+                ctx.beginPath();
+                ctx.rect(level.bricks[col][row].x, level.bricks[col][row].y, level.brickWidth, level.brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            };
+        };
+    };
+};
+
 // Add event listeners on player's actions
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
@@ -101,8 +151,8 @@ function keyDownHandler(event) {
     }
     else if (event.key == 'ArrowLeft' || event.key == 'Left') {
         paddle.move.left = true;
-    }
-}
+    };
+};
 
 function keyUpHandler(event) {
     if (event.key == 'ArrowRight' || event.key == 'Right') {
@@ -110,25 +160,45 @@ function keyUpHandler(event) {
     }
     else if (event.key == 'ArrowLeft' || event.key == 'Left') {
         paddle.move.left = false;
-    }
-}
+    };
+};
 
 // Collisions handler
-function collisionHandler() {
+function wallsCollisionHandler() {
     if (ball.position.x + ball.speed.dx > field.size.x - ball.size || ball.position.x + ball.speed.dx < ball.size) {
         ball.speed.dx = -ball.speed.dx;
     };
     if (ball.position.y + ball.speed.dy < ball.size || (
         ball.position.y + ball.speed.dy > field.size.y - paddle.size.y - ball.size &&
         ball.position.x + ball.speed.dx > paddle.position.x &&
-        ball.position.x + ball.speed.dx < paddle.position.x + paddle.size.x)
-    ) {
-        ball.speed.dy = -ball.speed.dy;
+        ball.position.x + ball.speed.dx < paddle.position.x + paddle.size.x)) {
+            ball.speed.dy = -ball.speed.dy;
     }
     else if (ball.position.y + ball.speed.dy > field.size.y) {
         location.reload();
     };
-}
+};
+
+function brickCollisionHandler() {
+    for (let col=0; col<level.brickColumnCount; col++) {
+        for (let row=0; row<level.brickRowCount; row++) {
+            let brick = level.bricks[col][row];
+            if (brick.strength > 0 &&
+                ball.position.x - ball.size + ball.speed.dx < brick.x + level.brickWidth &&
+                ball.position.x + ball.size + ball.speed.dx > brick.x &&
+                ball.position.y - ball.size + ball.speed.dy < brick.y + level.brickHeight &&
+                ball.position.y + ball.size + ball.speed.dy > brick.y) {
+                    if (ball.position.x > brick.x + level.brickWidth ||
+                        ball.position.x < brick.x) {
+                            ball.speed.dx = -ball.speed.dx;
+                    }
+                    else ball.speed.dy = -ball.speed.dy;
+                    brick.strength -= 1;
+                    gameState.score += 10;
+            };
+        };
+    };
+};
 
 // Game Loop
 function gameLoop() {
@@ -137,10 +207,13 @@ function gameLoop() {
     drawField();
     drawPaddle();
     drawBall();
+    drawGameState();
+    drawBricks();
 
     // Objects movement and collision detection
-    collisionHandler()
-    
+    wallsCollisionHandler();
+    brickCollisionHandler();
+
     paddle.movePaddle();
     ball.moveBall();
 
